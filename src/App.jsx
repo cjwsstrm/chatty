ea247749d59f8269ac954b723d007c48fef2eb7f
cjwsstrm <cjwesstrom@gmail.com>
2017-10-03 02:54:26 +0000

    created and populated files for base ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+
 
 class App extends Component {
   render() {
-    return (                  // insert <div> before h1 to wrap everything.
-      <h1>Hello React :)</h1> // There can only be one root <>, so make it something
-    );                        // ambiguous like a div
+    console.log("Rendering <App/>");
+    return (
+      <div>
+        <nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+        </nav>           
+        <MessageList/>
+        <ChatBar/> 
+      </div>
+    );                        
   }
 }
 export default App;
---
+++ src/MessageList.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    console.log("Rendering <MessageList/>");
+    return (     
+        <div>            
+            <main className="messages">
+                <Message/>
+            </main> 
+        </div>
+    );                       
+  }
+}
+
+export default MessageList;
