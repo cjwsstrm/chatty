54f3be2593d5f34eda15517827e3f2c2730adfa1
cjwsstrm <cjwesstrom@gmail.com>
2017-10-03 01:21:51 +0000

    minor linting and starting comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
+    return (                  // insert <div> before h1 to wrap everything.
+      <h1>Hello React :)</h1> // There can only be one root <>, so make it something
+    );                        // ambiguous like a div
   }
 }
 export default App;
